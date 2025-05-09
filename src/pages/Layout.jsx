d1a03fe31caf568a13775c1859703bcdb6fe8eed
Saddam-tech <salokhiddinov0727@gmail.com>
2025-05-09 23:48:44 +0900

    merged remote main with local and fixed conflicts

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,7 +1,6 @@
 import { Routes, Route, useLocation } from "react-router-dom";
 import Register from "./Register";
 import Main from "./Main";
-import SignUp from "./SignUp";
 import Applicant from "./Applicant";
 import AccauntSetting from "./AccauntSetting";
 
@@ -16,7 +15,6 @@ function Layout() {
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/signup" element={<SignUp />}></Route>
         <Route path="/applicant" element={<Applicant />} />
         <Route path="/accauntSetting" element={<AccauntSetting />} />
         <Route path="/supabase-signup" element={<SupabaseSignUp />}></Route>
