a73b42c5e329276fe73acd93db52e25504af0565
Rakhmonali7 <zamonalievr7@gmail.com>
2025-05-13 05:50:53 +0900

    job details success modal has been created

--- src/components/JobDetails.jsx
+++ src/components/JobDetails.jsx
@@ -1,8 +1,19 @@
 import globeIcon from '../assets/globe.svg';
 import dollarIcon from '../assets/dollar-sign.svg';
+import { Modal } from 'antd';
 import Button from './Button';
+import { useState } from 'react';
 
 function JobDetails({ job, onBack }) {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
+
+  const handleOk = () => {
+    setIsModalVisible(false);
+  };
   return (
     <div className="p-8 space-y-4 bg-white rounded-lg shadow-md">
       <button
@@ -63,9 +74,22 @@ function JobDetails({ job, onBack }) {
         <span className="block mt-4 text-sm text-gray-600">{job.message}</span>
       )}
 
-      <button className="mt-6 px-4 py-2 bg-gray-950 text-white rounded hover:bg-gray-600 transition">
+      <button
+        onClick={showModal}
+        className="mt-6 px-4 py-2 bg-gray-950 text-white rounded hover:bg-gray-600 transition"
+      >
         Apply
       </button>
+      <Modal
+        title="✅ Application Submitted Successfully"
+        visible={isModalVisible}
+        onOk={handleOk}
+        okText="OK"
+        closable={false}
+        cancelButtonProps={{ style: { display: 'none' } }}
+      >
+        <p>You will receive a confirmation email shortly.</p>
+      </Modal>
     </div>
   );
 }
